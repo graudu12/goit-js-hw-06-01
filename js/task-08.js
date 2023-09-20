72d1d3701f08556369d74e3a421912a03b133145
Dmitrijs <graudu_12@yahoo.co.uk>
2023-09-20 18:47:08 +0100

    upd_06_08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', allSubmit)
+
+function allSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!')
+    }
+    const userData = { email: email.value, Password: password.value }
+    
+    //console.log(userData)
+    event.currentTarget.reset()
+}
