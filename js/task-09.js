61c8fd631920667a36c22ea458d2d046e3812ffb
Dmitrijs <graudu_12@yahoo.co.uk>
2023-09-20 19:22:58 +0100

    upd_06_09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColor = document.querySelector('button.change-color')
+const body = document.querySelector('body')
+const colorVal = document.querySelector('span.color')
+
+
+changeColor.addEventListener('click', a => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorVal.textContent = color;
+})
